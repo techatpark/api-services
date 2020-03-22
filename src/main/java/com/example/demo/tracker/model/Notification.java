7291512d64af800dbfa4b3800c4e3712ace5f1a3
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-22 19:47:47 +0800

    model fixes

--- src/main/java/com/example/demo/tracker/model/Notification.java
+++ src/main/java/com/example/demo/tracker/model/Notification.java
@@ -1,217 +1,136 @@
 package com.example.demo.tracker.model;
 
 public class Notification extends BaseModel {
-    /**
-<<<<<<< HEAD
-     * refference code of notification.
-     */
-    private String refferenceCode;
-    /**
-     * address of participant.
-     */
-    private String participantAddress;
-    /**
-     * count of transactions.
-     */
-    private Integer transactionCount;
-    /**
-     * tells the request log.
-     */
-    private String requestLog;
-    /**
-     * tells response log.
-     */
-    private String responseLog;
-    /**
-     * mode of notification.
-=======
-     * this is refference Code.
-     */
-    private String refferenceCode;
-    /**
-     * this is the participant Address.
-     */
-    private String participantAddress;
-    /**
-     * this is the no. of transactions.
-     */
-    private Integer transactionCount;
-    /**
-     * This shows the request log.
-     */
-    private String requestLog;
-    /**
-     * This is response log.
-     */
-    private String responseLog;
-    /**
-     * This is notification Mode.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     */
-    private String notificationMode;
 
-    /**
-<<<<<<< HEAD
-     * gets refference code.
-     * 
-     * @return refference code
-=======
-     * This method let's us get Reference code.
-     * 
-     * @return reffernce code
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     */
-    public String getRefferenceCode() {
-        return refferenceCode;
-    }
+     /**
+      * mode of notification. ======= this is refference Code.
+      */
+     private String refferenceCode;
+     /**
+      * this is the participant Address.
+      */
+     private String participantAddress;
+     /**
+      * this is the no. of transactions.
+      */
+     private Integer transactionCount;
+     /**
+      * This shows the request log.
+      */
+     private String requestLog;
+     /**
+      * This is response log.
+      */
+     private String responseLog;
+     /**
+      * This is notification Mode.
+      */
+     private String notificationMode;
 
-    /**
-<<<<<<< HEAD
-     * sets refference code.
-=======
-     * to set reffernce code.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param refferenceCode
-     */
-    public void setRefferenceCode(final String refferenceCode) {
-        this.refferenceCode = refferenceCode;
-    }
+     /**
+      * gets refference code.
+      * 
+      * @return refference code
+      */
+     public String getRefferenceCode() {
+          return refferenceCode;
+     }
 
-    /**
-<<<<<<< HEAD
-     * gets participant address.
-     * 
-     * @return participant address
-=======
-     * This is to get particiapnt address.
-     * 
-     * @return participantAdress
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     */
-    public String getParticipantAddress() {
-        return participantAddress;
-    }
+     /**
+      * sets refference code.
+      * 
+      * @param refferenceCode
+      */
+     public void setRefferenceCode(final String refferenceCode) {
+          this.refferenceCode = refferenceCode;
+     }
 
-    /**
-<<<<<<< HEAD
-     * sets participant address.
-=======
-     * This let's us set participant address.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param participantAddress
-     */
-    public void setParticipantAddress(final String participantAddress) {
-        this.participantAddress = participantAddress;
-    }
+     /**
+      * gets participant address.
+      * 
+      * @return participant address
+      */
+     public String getParticipantAddress() {
+          return participantAddress;
+     }
 
-    /**
-<<<<<<< HEAD
-     * gets transaction count.
-     * 
-     * @return transaction count
-=======
-     * This is to get transcation Count.
-     * 
-     * @return transcation count
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     */
-    public int getTransactionCount() {
-        return transactionCount;
-    }
+     /**
+      * This let's us set participant address.
+      * 
+      * @param participantAddress
+      */
+     public void setParticipantAddress(final String participantAddress) {
+          this.participantAddress = participantAddress;
+     }
 
-    /**
-<<<<<<< HEAD
-     * sets transaction count.
-=======
-     * This is to set transaction count.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param transactionCount
-     */
-    public void setTransactionCount(final int transactionCount) {
-        this.transactionCount = transactionCount;
-    }
+     /**
+      * gets transaction count.
+      * 
+      * @return transcation count
+      */
+     public int getTransactionCount() {
+          return transactionCount;
+     }
 
-    /**
-<<<<<<< HEAD
-     * gets the request log.
-=======
-     * This is to get the request log.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @return request log
-     */
-    public String getRequestLog() {
-        return requestLog;
-    }
+     /**
+      * This is to set transaction count.
+      * 
+      * @param transactionCount
+      */
+     public void setTransactionCount(final int transactionCount) {
+          this.transactionCount = transactionCount;
+     }
 
-    /**
-<<<<<<< HEAD
-     * sets request log.
-=======
-     * This is to set request log.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param requestLog
-     */
-    public void setRequestLog(final String requestLog) {
-        this.requestLog = requestLog;
-    }
+     /**
+      * This is to get the request log.
+      * 
+      * @return request log
+      */
+     public String getRequestLog() {
+          return requestLog;
+     }
 
-    /**
-<<<<<<< HEAD
-     * gets response log.
-=======
-     * This is to get respinse log.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @return response log
-     */
-    public String getResponseLog() {
-        return responseLog;
-    }
+     /**
+      * This is to set request log.
+      * 
+      * @param requestLog
+      */
+     public void setRequestLog(final String requestLog) {
+          this.requestLog = requestLog;
+     }
 
-    /**
-<<<<<<< HEAD
-     * sets response log.
-=======
-     * this is to set response log.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param responseLog
-     */
-    public void setResponseLog(final String responseLog) {
-        this.responseLog = responseLog;
-    }
+     /**
+      * This is to get respinse log.
+      * 
+      * @return response log
+      */
+     public String getResponseLog() {
+          return responseLog;
+     }
 
-    /**
-<<<<<<< HEAD
-     * gets the notification mode.
-     * 
-     * @return notification mode
-=======
-     * this is to get Notification Mode.
-     * 
-     * @return getNotificationMode
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     */
-    public String getNotificationMode() {
-        return notificationMode;
-    }
+     /**
+      * sets response log.
+      * 
+      * @param responseLog
+      */
+     public void setResponseLog(final String responseLog) {
+          this.responseLog = responseLog;
+     }
 
-    /**
-<<<<<<< HEAD
-     * sets notification mode.
-=======
-     * This is to set Notification mode to something.
->>>>>>> d06fc258c77dd3f06c79fc9411e398a78ff32d1b
-     * 
-     * @param notificationMode
-     */
-    public void setNotificationMode(final String notificationMode) {
-        this.notificationMode = notificationMode;
-    }
+     /**
+      * @return notification mode ======= this is to get Notification Mode.
+      */
+     public String getNotificationMode() {
+          return notificationMode;
+     }
+
+     /**
+      * to something.
+      * 
+      * @param notificationMode
+      */
+     public void setNotificationMode(final String notificationMode) {
+          this.notificationMode = notificationMode;
+     }
 
 }
