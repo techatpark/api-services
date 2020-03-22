7fa1af8bf631ecaada58a7284c66b301b564986d
dkarthicks27 <dkarthicks27@gmail.com>
2020-03-22 11:03:27 +0530

    commit

---
+++ src/main/java/com/example/demo/tracker/model/BaseModel.java
@@ -0,0 +1,31 @@
+package com.example.demo.tracker.model;
+
+public class BaseModel {
+    private Integer id;
+    private String code;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
