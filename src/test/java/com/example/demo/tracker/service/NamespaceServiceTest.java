8612f36dca5822811630175973594c314de9cbc8
rmhari <rm.haripriya@gmail.com>
2020-03-23 20:23:55 +0530

    namespace service test

--- src/test/java/com/example/demo/tracker/service/NamespaceServiceTest.java
+++ src/test/java/com/example/demo/tracker/service/NamespaceServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.tracker.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.example.demo.tracker.model.Namespace;
 import com.example.demo.tracker.model.Status;
@@ -39,6 +41,33 @@ class NamespaceServiceTest {
         namespace.setCode("12a3");
         namespace.setName("KPN");
         return namespace;
+    }
+
+    @Test
+    void testDelete() {
+        Namespace namespace = namespaceService.create(getNamespaceForTesting());
+        Integer newNamespaceId = namespace.getId();
+        assertNotNull(namespaceService.read(newNamespaceId), "Created Device exists before delete");
+        namespaceService.delete(newNamespaceId);
+        assertNull(namespaceService.read(newNamespaceId), "Created Device does not exist after delete");
+    }
+
+    @Test
+    void testUpdate() {
+        Namespace namespace = namespaceService.create(getNamespaceForTesting());
+        Integer newNamespaceId = namespace.getId();
+        namespace.setName("Updated Name");
+        namespaceService.update(newNamespaceId, namespace);
+        assertEquals("Updated Name", namespace.getName(), "Updated");
+    }
+
+    @Test
+    void testList() {
+        namespaceService.create(getNamespaceForTesting());
+        Namespace namespace2 = getNamespaceForTesting();
+        namespace2.setName("KPN");
+        namespaceService.create(namespace2);
+        assertEquals(2, namespaceService.list().size(), "Test Listing");
     }
 
 }
