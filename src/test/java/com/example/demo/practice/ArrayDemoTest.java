d83f5174fcbbd605db29ef54d9e8b8248d4a7e68
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-27 21:53:23 +0800

    start practice

---
+++ src/test/java/com/example/demo/practice/ArrayDemoTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.practice;
+
+public final class ArrayDemoTest {
+
+    public static void main(String[] arguments) {
+
+        // Declare
+        Integer[] anArray;
+
+        anArray = new Integer[10];
+
+        anArray[0] = 2;
+        anArray[1] = 3;
+
+        System.out.println("Hi Java");
+    }
+
+}
