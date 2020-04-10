c7873c08fb1403128751269acfb06fd84ee34bc4
rmhari <rm.haripriya@gmail.com>
2020-04-10 12:54:39 +0530

    Chumma

--- src/main/java/com/example/demo/sql/controller/QuestionAPIController.java
+++ src/main/java/com/example/demo/sql/controller/QuestionAPIController.java
@@ -1,19 +1,12 @@
 package com.example.demo.sql.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import com.example.demo.tracker.model.Namespace;
-import com.example.demo.tracker.service.NamespaceService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
