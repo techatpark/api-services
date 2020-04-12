f0a62899edd6d8ce65d8fe3cd8f71d055d4d6f4d
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-04-12 13:02:22 +0800

    try

--- src/main/java/com/example/demo/sql/service/AnswerService.java
+++ src/main/java/com/example/demo/sql/service/AnswerService.java
@@ -1,9 +1,26 @@
 package com.example.demo.sql.service;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnswerService {
+
+    /**
+     * * this is used to execute a connection with a database.
+     * 
+     */
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * Constructs Answer Service.
+     * 
+     * @param jdbcTemplate
+     */
+    AnswerService(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     /**
      * checks whether the given answer is correct.returns true if correct.
      * 
@@ -12,6 +29,7 @@ public class AnswerService {
      * @return true
      */
     public final Boolean answer(final Integer questionId, final String answer) {
+
         return true;
     }
 
