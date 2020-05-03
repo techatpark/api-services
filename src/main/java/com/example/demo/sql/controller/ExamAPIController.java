2a2b16efe92a3ba683a519d0d9f087a97945a4d9
rajaduraikrishna <k.rajadurai@gmail.com>
2020-05-03 19:00:49 +0530

    Added find, update and delete operations

--- src/main/java/com/example/demo/sql/controller/ExamAPIController.java
+++ src/main/java/com/example/demo/sql/controller/ExamAPIController.java
@@ -1,5 +1,7 @@
 package com.example.demo.sql.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.example.demo.sql.model.Exam;
@@ -7,7 +9,11 @@ import com.example.demo.sql.service.ExamService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,4 +44,38 @@ class ExamAPIController {
         return ResponseEntity.status(HttpStatus.CREATED).body(examService.create(exam));
     }
 
+    @ApiOperation(value = "Get exam with given id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "exam"),
+            @ApiResponse(code = 404, message = "exam not found") })
+    @GetMapping("/{id}")
+    public ResponseEntity<Exam> findById(@PathVariable Integer id) {
+        return ResponseEntity.of(examService.read(id));
+    }
+
+    @ApiOperation(value = "Updates the exam by given id", notes = "Can be called only by users with 'auth management' rights.")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "exam updated successfully"),
+            @ApiResponse(code = 400, message = "exam is invalid"),
+            @ApiResponse(code = 404, message = "exam not found") })
+    @PutMapping("/{id}")
+    public ResponseEntity<Exam> update(@PathVariable Integer id, @Valid @RequestBody Exam exam) {
+        Exam updatedexam = examService.update(id, exam);
+        return updatedexam == null ? new ResponseEntity<Exam>(HttpStatus.NOT_FOUND) : ResponseEntity.ok(updatedexam);
+    }
+
+    @ApiOperation(value = "Deletes the exam by given id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "exam deleted successfully"),
+            @ApiResponse(code = 404, message = "exam not found") })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        return examService.delete(id) ? ResponseEntity.ok().build() : new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+    }
+
+    @ApiOperation(value = "lists all the exam", notes = "Can be Invoked by auth users only")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Listing all the exam"),
+            @ApiResponse(code = 204, message = "exam are not available") })
+    @GetMapping
+    public ResponseEntity<List<Exam>> findAll() {
+        List<Exam> exams = examService.lists(1, 1);
+        return exams.isEmpty() ? new ResponseEntity<List<Exam>>(HttpStatus.NO_CONTENT) : ResponseEntity.ok(exams);
+    }
 }
--- src/main/java/com/example/demo/sql/service/ExamService.java
+++ src/main/java/com/example/demo/sql/service/ExamService.java
@@ -2,6 +2,7 @@ package com.example.demo.sql.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.sql.model.Exam;
 
@@ -26,4 +27,49 @@ public class ExamService {
         this.exams.add(exam);
         return exam;
     }
+
+    /**
+     * reads from table exame.
+     * 
+     * @param id
+     * @return namespace
+     */
+    public Optional<Exam> read(final Integer id) {
+        Optional<Exam> exam = this.exams.stream().filter(aExam -> id == aExam.getId()).findFirst();
+        return exam;
+    }
+
+    /**
+     * update table exam.
+     * 
+     * @param id
+     * @param exam
+     * @return exam
+     */
+    public Exam update(final Integer id, final Exam exam) {
+        this.exams.set(id - 1, exam);
+        return exam;
+    }
+
+    /**
+     * delete exam.
+     * 
+     * @param id
+     * @return exam
+     */
+    public boolean delete(final Integer id) {
+        return this.exams.remove(id - 1) != null;
+    }
+
+    /**
+     * delete exam.
+     * 
+     * @param pageNumber
+     * @param pageSize
+     * @return exam
+     */
+    public List<Exam> lists(final Integer pageNumber, final Integer pageSize) {
+        return this.exams;
+    }
+
 }
