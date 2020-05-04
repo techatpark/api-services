6f71c56af0fa71d2eaf92fbea2c0db8b6cc296f5
rmhari <31511082+rmhari@users.noreply.github.com>
2020-05-04 09:55:40 +0530

    Updated recent ddl

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -5,41 +5,18 @@ CREATE TABLE exams (
 );
 DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
-  id SERIAL PRIMARY KEY,
-  exam_id int NOT NULL UNIQUE,
+  id SERIAL ,
+  exam_id int NOT NULL,
   question VARCHAR(500) NOT NULL,
   answer VARCHAR(500) NOT NULL,
+	PRIMARY KEY (id, exam_id),
   FOREIGN KEY (exam_id) REFERENCES exams (id)
 );
 DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
   id SERIAL PRIMARY KEY,
-  exam_id int NOT NULL,
+  exam_id int NOT NULL ,
   question_id int NOT NULL,
   student_answer VARCHAR(500) NOT NULL,
-  FOREIGN KEY (exam_id) REFERENCES exams (id),
-  FOREIGN KEY (question_id) REFERENCES questions (id)
-);
-
-DROP TABLE IF EXISTS exams;
-CREATE TABLE exams (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(55) NOT NULL
-);
-DROP TABLE IF EXISTS questions;
-CREATE TABLE questions (
-  id SERIAL PRIMARY KEY,
-  exam_id int NOT NULL,
-  question VARCHAR(500) NOT NULL,
-  answer VARCHAR(500) NOT NULL,
-  FOREIGN KEY (exam_id) REFERENCES exams (id)
-);
-DROP TABLE IF EXISTS answers;
-CREATE TABLE answers (
-  id SERIAL PRIMARY KEY,
-  exam_id int NOT NULL,
-  question_id int NOT NULL,
-  student_answer VARCHAR(500) NOT NULL,
-  FOREIGN KEY (exam_id) REFERENCES exams (id),
-  FOREIGN KEY (question_id) REFERENCES questions (id)
+ FOREIGN KEY (exam_id, question_id) REFERENCES questions (exam_id, id)
 );
