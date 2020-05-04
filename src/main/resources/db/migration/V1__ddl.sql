10e929c75a304c79e6212876d468d53b8aa66ab4
rmhari <31511082+rmhari@users.noreply.github.com>
2020-05-04 09:36:56 +0530

    Updated question ddl

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -20,3 +20,26 @@ CREATE TABLE answers (
   FOREIGN KEY (exam_id) REFERENCES exams (id),
   FOREIGN KEY (question_id) REFERENCES questions (id)
 );
+
+DROP TABLE IF EXISTS exams;
+CREATE TABLE exams (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55) NOT NULL
+);
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  exam_id int NOT NULL,
+  question VARCHAR(500) NOT NULL,
+  answer VARCHAR(500) NOT NULL,
+  FOREIGN KEY (exam_id) REFERENCES exams (id)
+);
+DROP TABLE IF EXISTS answers;
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  exam_id int NOT NULL,
+  question_id int NOT NULL,
+  student_answer VARCHAR(500) NOT NULL,
+  FOREIGN KEY (exam_id) REFERENCES exams (id),
+  FOREIGN KEY (question_id) REFERENCES questions (id)
+);
