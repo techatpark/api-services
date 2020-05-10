404bcdf7521703ebfcd1837304c5775fd1d4b630
rmhari <31511082+rmhari@users.noreply.github.com>
2020-05-10 17:05:57 +0530

    dummy

---
+++ src/main/java/com/example/demo/sql/service/DatabaseConnector.java
@@ -0,0 +1,25 @@
+package com.example.demo.sql.service;
+
+import com.example.demo.sql.model.Exam;
+import com.example.demo.sql.model.Question;
+
+import java.io.File;
+
+
+
+
+public interface DatabaseConnector {
+   /**
+    * verify the given question with the answer.
+    * @param exam
+    * @param question
+    * @param sqlAnswer
+    * @return successflag
+    */
+    Boolean verify(Exam exam, Question question, String sqlAnswer);
+    /**
+     * Load the script for the specific exam.
+     * @return successflag
+     */
+    Boolean loadScript(Exam exam, File[] scriptFiles);
+}
