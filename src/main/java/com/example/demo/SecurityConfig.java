23be846e460bbdf8d15ceb9581d224c77d343f90
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-05-24 13:59:58 +0800

    file usploade orks

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().anyRequest().permitAll();
+		http.csrf().disable().httpBasic().and().authorizeRequests().anyRequest().permitAll();
 	}
 
 }
