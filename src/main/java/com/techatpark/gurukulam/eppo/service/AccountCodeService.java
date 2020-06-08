43e4074ebd3e0a12759e69a2e968cda2d90b0023
rm.hari <rm.haripriya@gmail.com>
2020-06-08 20:29:23 +0530

    dummy snippet

--- src/main/java/com/techatpark/gurukulam/eppo/service/AccountCodeService.java
+++ src/main/java/com/techatpark/gurukulam/eppo/service/AccountCodeService.java
@@ -1,41 +1,39 @@
-package com.techatpark.gurukulam.eppo.model;
+package com.techatpark.gurukulam.eppo.service;
 
 import javax.sql.DataSource;
-
 import com.techatpark.gurukulam.eppo.model.AccountCode;
-
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
-
 @Service
 public class AccountCodeService {
-    /**
-     * this is used to execute a connection with a database.
-     */
-    private final JdbcTemplate jdbcTemplate;
-    /**
-     * this is used to connect to relational database.
-     */
-    private final DataSource dataSource;
-
-    /**
-     * Creates a service for AccountCode related operations.
-     * 
-     * @param jdbcTemplate
-     * @param dataSource
-     */
-    public AccountCodeService(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    /**
-     * inserting into AccountCode table.
-     * 
-     * @param newAccountCode
-     * @return reads the input data
-     */
-    public AccountCode create(final AccountCode newAccountCode) {
-        return read(id.intValue()).get();
-    }
+/**
+* this is used to execute a connection with a database.
+*/
+private final JdbcTemplate jdbcTemplate;
+/**
+* this is used to connect to relational database.
+*/
+private final DataSource dataSource;
+/** 
+* Creates a service for AccountCode related operations.
+* @param jdbcTemplate
+* @param dataSource
+*/
+public AccountCodeService(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
+this.dataSource = dataSource;
+this.jdbcTemplate = jdbcTemplate;
 }
+/**
+* inserting into AccountCode table.
+* 
+* @param newAccountCode
+* @return reads the input data
+*/
+public AccountCode create(final AccountCode newAccountCode) {
+final SimpleJdbcInsert insert = new SimpleJdbcInsert(dataSource).withTableName("account_codes")
+.usingGeneratedKeyColumns("id").usingColumns("account_code", "code_used");
+return read(id.intValue()).get();
+}
+}
+  
