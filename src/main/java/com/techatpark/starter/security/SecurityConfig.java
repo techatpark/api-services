4fd5c0e58a06b4530891ecaf25283319047d8b04
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-03-17 16:04:17 +0530

    sss

--- src/main/java/com/techatpark/starter/security/SecurityConfig.java
+++ src/main/java/com/techatpark/starter/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
