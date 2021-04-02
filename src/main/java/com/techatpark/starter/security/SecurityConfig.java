9f317fcf342f0a53cea17dd42551bb0ba309fdbb
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-04-02 16:23:47 +0530

    please ya 8

--- src/main/java/com/techatpark/starter/security/SecurityConfig.java
+++ src/main/java/com/techatpark/starter/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public SecurityConfig(TokenUtil tokenUtil) {
         this.tokenUtil = tokenUtil;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**");
     }
 
     /**
