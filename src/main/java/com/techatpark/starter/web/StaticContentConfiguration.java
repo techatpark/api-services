c8d2a7d9967831beacc545776266a2bea09ce856
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-04-03 07:09:52 +0530

    static

---
+++ src/main/java/com/techatpark/starter/web/StaticContentConfiguration.java
@@ -0,0 +1,19 @@
+package com.techatpark.starter.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class StaticContentConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        List.of("/courses/c-programming"
+                ,"/practices/sql").stream().forEach(path->{
+            registry.addViewController(path).setViewName("forward:"+path+"/index.html");
+        });
+    }
+}
