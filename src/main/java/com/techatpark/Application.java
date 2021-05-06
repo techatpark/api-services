e8a15dabd42875342f50a77acfcb833890f0efc3
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-05-06 11:30:35 +0530

    fx issue

--- src/main/java/com/techatpark/Application.java
+++ src/main/java/com/techatpark/Application.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class Application {
+public class Application {
 
-    private Application() {
+    /**
+     * Workaround to fix Checkstyle Issue.
+     * Ref: https://github.com/checkstyle/checkstyle/issues/3155
+     */
+    public void s() {
     }
+
     /**
      * Main method of this application.
      *
      * @param args
      */
+
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
