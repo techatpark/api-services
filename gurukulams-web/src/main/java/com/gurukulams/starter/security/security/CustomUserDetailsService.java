eacdfae06d97419c10406992947eb4d9ac553deb
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-09-22 20:27:35 +0530

    Maven Profile Introduced

--- gurukulams-web/src/main/java/com/gurukulams/starter/security/security/CustomUserDetailsService.java
+++ gurukulams-web/src/main/java/com/gurukulams/starter/security/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.gurukulams.starter.security.security;
 
 
 import com.gurukulams.starter.security.model.User;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,22 +29,28 @@ public class CustomUserDetailsService implements UserDetailsService {
      * Builds the Object.
      *
      * @param passwordEncoder the password encoder
+     * @param environment the environment
      */
-    public CustomUserDetailsService(final PasswordEncoder passwordEncoder) {
+    public CustomUserDetailsService(final PasswordEncoder passwordEncoder,
+                                    final Environment environment) {
         this.users = new ArrayList<>();
 
-        User user = new User();
-        user.setName("tom");
-        user.setPassword(passwordEncoder.encode("password"));
-        user.setImageUrl("/images/" + user.getName() + ".png");
+        if (Arrays.binarySearch(environment.getActiveProfiles(),
+                "prod") == -1) {
+            User user = new User();
+            user.setName("tom");
+            user.setPassword(passwordEncoder.encode("password"));
+            user.setImageUrl("/images/" + user.getName() + ".png");
 
-        users.add(user);
+            users.add(user);
 
-        user = new User();
-        user.setName("jerry");
-        user.setPassword(passwordEncoder.encode("password"));
-        user.setImageUrl("/images/" + user.getName() + ".png");
-        users.add(user);
+            user = new User();
+            user.setName("jerry");
+            user.setPassword(passwordEncoder.encode("password"));
+            user.setImageUrl("/images/" + user.getName() + ".png");
+            users.add(user);
+        }
+
 
     }
 
