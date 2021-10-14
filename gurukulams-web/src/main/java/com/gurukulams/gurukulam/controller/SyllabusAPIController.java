0bf6ee91089a2f4580a006deb7a03ef41e9e4561
Manikanta445 <manikantayarramsetti445@gmail.com>
2021-10-14 22:21:42 +0530

    syllabus api

--- gurukulams-web/src/main/java/com/gurukulams/gurukulam/controller/SyllabusAPIController.java
+++ gurukulams-web/src/main/java/com/gurukulams/gurukulam/controller/SyllabusAPIController.java
@@ -1,6 +1,5 @@
 package com.gurukulams.gurukulam.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gurukulams.gurukulam.model.Syllabus;
 import com.gurukulams.gurukulam.service.SyllabusService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,138 +9,79 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletException;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Optional;
 
+/**
+ * The type Syllabus api controller.
+ */
 @RestController
 @RequestMapping("/api/syllabus")
 @Tag(name = "Syllabus", description = "Resource to manage Syllabus")
 class SyllabusAPIController {
     /**
-     * declare a bookservice.
+     * declare a syllabusService.
      */
     private final SyllabusService syllabusService;
 
-    /**
-     * Instantiates a new Book api controller.
-     * @param asyllabusService a syllabus service
-     */
     SyllabusAPIController(final SyllabusService asyllabusService) {
         this.syllabusService = asyllabusService;
     }
 
     /**
      * Create response entity.
-     * @param id the id
-     * @param title the title
-     * @param description the description.
+     *
+     * @param principal the principal
+     * @param syllabus  the syllabus
+     * @return the response entity
+     * @throws IOException the io exception
+     */
+    @Operation(summary = "Creates a new syllabus", description =
+            "Can be called only by users with 'auth management' rights.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @PostMapping
+    public ResponseEntity<Optional<Syllabus>> create(final Principal principal,
+                                              final
+                                              @RequestBody
+                                                      Syllabus
+                                                      syllabus)
+            throws IOException {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(syllabusService.create(principal.getName(), syllabus));
+    }
+
+    /**
+     * Create response entity.
+     * @param userName the userName
+     * @param syllabus the syllabus
      * @return the response entity
      */
-    @Operation(summary = "Creates a new syllabus",
+    @Operation(summary = "Creates a new Syllabus",
             description = "Can be called only by users with"
                     + " 'auth management' rights.",
             security = @SecurityRequirement(name = "bearerAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "201",
-            description = "question created successfully"),
+            description = "syllabus created successfully"),
             @ApiResponse(responseCode = "400",
-                    description = "question is invalid"),
+                    description = "syllabus is not invalid"),
             @ApiResponse(responseCode = "401",
                     description = "invalid credentials")})
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{id}/{title}/description")
-    public ResponseEntity<Optional<Syllabus>> create(final @PathVariable
-                                                             long id,
-                                                     final @PathVariable
-                                                           String title,
-                                                     final String
-                                                             description)
-            throws ServletException, IOException {
+    @PostMapping("/userName/syllabus")
+    public ResponseEntity<Optional<Syllabus>> create(final @RequestBody
+                                                             String userName,
+                                                     final @RequestBody
+                                                         Syllabus syllabus) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                syllabusService.create(id, title, description));
-    }
-
-    /**
-     * Find syllabus by id response entity.
-     *
-     * @param id the id
-     * @return the response entity
-     */
-    @Operation(summary = "Get syllabus with given id",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponses(value = {@ApiResponse(responseCode = "200",
-            description = "syllabus"),
-            @ApiResponse(responseCode = "401",
-                    description = "invalid credentials"),
-            @ApiResponse(responseCode = "404",
-                    description = "syllabus not found")})
-    @GetMapping("/title/description/{id}")
-    public ResponseEntity<Syllabus> findQuestionById(final @PathVariable
-                                                             Integer id) {
-        return ResponseEntity.of(syllabusService.read(id));
-    }
-
-    /**
-     * Update response entity.
-     * @param id the id
-     * @param title the title
-     * @param description the description.
-     * @return the response entity
-     */
-    @Operation(summary = "Updates the syllabus by given id",
-            description = "Can be called only by users "
-                    + "with 'auth management' rights.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponses(value = {@ApiResponse(responseCode = "200",
-            description = "syllabus updated successfully"),
-            @ApiResponse(responseCode = "401",
-                    description = "invalid credentials"),
-            @ApiResponse(responseCode = "404",
-                    description = "syllabus not found")})
-    @PutMapping("/{id}/{title}/description")
-    public ResponseEntity<Optional<Syllabus>> update(final @PathVariable
-                                                             long id,
-                                                     final @PathVariable
-                                                             String title,
-                                                     final String description)
-            throws JsonProcessingException {
-
-        final Optional<Syllabus> updateSyllabus = syllabusService.update(
-                                           id, title, description);
-        return updateSyllabus == null
-                ? new ResponseEntity<Optional<Syllabus>>(
-                HttpStatus.NOT_FOUND)
-                : ResponseEntity.ok(updateSyllabus);
-    }
-
-    /**
-     * Delete exam by id response entity.
-     *
-     * @param id the id
-     * @return the response entity
-     */
-    @Operation(summary = "Deletes the syllabus by given id",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponses(value = {@ApiResponse(responseCode = "200",
-            description = "syllabus deleted successfully"),
-            @ApiResponse(responseCode = "404",
-                    description = "syllabus not found"),
-            @ApiResponse(responseCode = "401",
-                    description = "invalid credentials")})
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSyllabusById(
-            final @PathVariable Integer id) {
-        return syllabusService.deleteById(id) ? ResponseEntity.ok().build()
-                : new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+                syllabusService.create(userName, syllabus));
     }
 }
 
--- gurukulams-web/src/main/java/com/gurukulams/gurukulam/service/SyllabusService.java
+++ gurukulams-web/src/main/java/com/gurukulams/gurukulam/service/SyllabusService.java
@@ -1,17 +1,8 @@
 package com.gurukulams.gurukulam.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gurukulams.gurukulam.model.Syllabus;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
-import javax.validation.Validator;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -21,123 +12,45 @@ import java.util.Optional;
 public class SyllabusService {
 
     /**
-     * Validator.
+     * inserts data.
+     * @param userName the userName
+     * @param syllabus the syllabus
+     * @return question optional
      */
-    private final Validator validator;
+    public Optional<Syllabus> create(final String userName,
+                                     final Syllabus syllabus) {
+        return null;
+    }
     /**
-     * this creates connection functionalities.
+     * reads from syllabus.
+     * @param userName the userName
+     * @param syllabus the syllabus
+     * @return question optional
      */
-    private final DataSource dataSource;
-    /**
-     * this helps to execute sql queries.
-     */
-    private final JdbcTemplate jdbcTemplate;
-
-    /**
-     * initializes.
-     * @param adataSource the data source
-     * @param avalidator the validator
-     * @param ajdbcTemplate the jdbc template.
-     */
-    public SyllabusService(final Validator avalidator,
-                           final DataSource adataSource,
-                           final JdbcTemplate ajdbcTemplate) {
-
-        this.validator = avalidator;
-        this.dataSource = adataSource;
-        this.jdbcTemplate = ajdbcTemplate;
+    public Optional<Syllabus> read(final String userName,
+                                   final Syllabus syllabus) {
+        return null;
     }
 
     /**
-     * Maps the data from and to the database. return syllabus.
+     * update the syllabus.
+     * @param userName the userName
+     * @param syllabus the syllabus
+     * @return question optional
      */
-    private final RowMapper<Syllabus> rowMapper = (rs, rowNum) -> {
-        final Syllabus syllabus = new Syllabus();
-        syllabus.setId(rs.getLong("id"));
-        syllabus.setTitle(rs.getString("title"));
-        syllabus.setDescription(rs.getString("description"));
-
-        return syllabus;
-    };
-
-    /**
-     * Create optional.
-     * @param id id
-     * @param title the title
-     * @param description the description
-     * @param <T> the t
-     * @return the optional
-     */
-    public <T extends Syllabus> Optional<Syllabus> create(final long id,
-                                                     final String title,
-                                                     final String description)
-        throws JsonProcessingException {
-        final SimpleJdbcInsert insert =
-                new SimpleJdbcInsert(dataSource).withTableName("syllabus")
-                        .usingGeneratedKeyColumns("id")
-                        .usingColumns("syllabusId", "title", "description");
-
-        final Map<String, Object> valueMap = new HashMap<>();
-        valueMap.put("syllabusId", id);
-        valueMap.put("title", Syllabus.getTitle());
-        valueMap.put("description", Syllabus.getDescription());
-        final Number syllabusId = insert.executeAndReturnKey(valueMap);
-        return read(syllabusId.intValue());
+    public Optional<Syllabus> update(final String userName,
+                                     final Syllabus syllabus) {
+        return null;
     }
 
     /**
-     * Read optional.
-     * @param <T> the t
-     * @param id the id
-     * @return the optional
+     * deletes from database.
+     * @param userName the userName
+     * @param syllabus the syllabus
+     * @return question optional
      */
-    public <T extends Syllabus> Optional<T> read(final long id) {
-        final String query = "SELECT id, title, description"
-                + "FROM syllabus WHERE id = ?";
-
-
-        try {
-            return (Optional<T>) Optional.of(jdbcTemplate
-                    .queryForObject(query, new Object[]{id}, rowMapper));
-        } catch (final EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
-    }
-
-    /**
-     * delete all records from syllabus with the title.
-     *
-     * @param id
-     * @return successflag boolean
-     */
-    public Boolean deleteById(final long id) {
-        final String query =
-                "DELETE FROM syllabus WHERE id = ?";
-        final Integer updatedRows = jdbcTemplate.update(query, id);
-        return !(updatedRows == 0);
-    }
-
-    /**
-     * Update note optional.
-     *
-     * @param id       the id
-     * @param title the title
-     * @param description the description
-     * @param <T> the t
-     * @return the optional
-     */
-    public  <T extends Syllabus> Optional<T> update(final long id,
-                                                    final String title,
-                                                    final String description)
-            throws JsonProcessingException {
-
-        final String query =
-                "UPDATE syllabus SET title = ?,"
-                        + "description = ? WHERE id = ?";
-        final Integer updatedRows =
-                jdbcTemplate.update(query, Syllabus.getTitle(),
-                        Syllabus.getDescription(), id);
-        return updatedRows == 0 ? null : read(id);
-
+    public Optional<Syllabus> delete(final String userName,
+                                     final Syllabus syllabus) {
+        return null;
     }
 }
