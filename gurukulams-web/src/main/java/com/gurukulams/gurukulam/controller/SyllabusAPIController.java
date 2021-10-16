e8457bfb7099df986f4935685a0fe50c1cf2839c
Manikanta445 <manikantayarramsetti445@gmail.com>
2021-10-16 15:34:28 +0530

    request mapping

--- gurukulams-web/src/main/java/com/gurukulams/gurukulam/controller/SyllabusAPIController.java
+++ gurukulams-web/src/main/java/com/gurukulams/gurukulam/controller/SyllabusAPIController.java
@@ -10,7 +10,15 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
@@ -45,11 +53,12 @@ class SyllabusAPIController {
                     + "only by users with 'auth management' rights.",
             security = @SecurityRequirement(name = "bearerAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "201",
-            description = "user note created successfully"),
+            description = "syllabus created successfully"),
             @ApiResponse(responseCode = "401",
-                    description = "invalid syllabus"),
+                    description = "syllabus is invalid"),
             @ApiResponse(responseCode = "400",
-                    description = "user note is invalid")})
+                    description = "invalid credentials")})
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<Optional<Syllabus>> create(final Principal principal,
                                         final @RequestBody Syllabus syllabus) {
@@ -63,12 +72,41 @@ class SyllabusAPIController {
      * @param syllabus the syllabus
      * @return syllabus optional
      */
+    @Operation(summary = "Get the Syllabus with given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "getting syllabus successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "syllabus not found")})
+
+    @GetMapping("/{id}/{title}/description")
     public ResponseEntity<Syllabus> read(final Principal principal,
-                                   final @RequestBody Syllabus syllabus) {
+                                   final @PathVariable Syllabus syllabus) {
         return ResponseEntity.of(syllabusService.read(principal.getName(),
                                                                 syllabus));
     }
 
+    /**
+     * updates the syllabus.
+     * @param principal the principal
+     * @param syllabus the syllabus
+     * @return syllabus optional
+     */
+    @Operation(summary = "Updates the syllabus by given id",
+            description = "Can be called only by users "
+                    + "with 'auth management' rights.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "syllabus updated successfully"),
+            @ApiResponse(responseCode = "400",
+                    description = "syllabus is invalid"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "syllabus not found")})
+    @PutMapping
     public ResponseEntity<Optional<Syllabus>> update(final Principal
                                                         principal,
                                                      final @RequestBody
@@ -87,17 +125,37 @@ class SyllabusAPIController {
      * @param id the id
      * @return syllabus optional
      */
+    @Operation(summary = "Deletes the syllabus by given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "syllabus deleted successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "syllabus not found")})
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(final @PathVariable
                                                long id) {
         return syllabusService.delete(id) ? ResponseEntity.ok().build()
                 : new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
+
     /**
      * list the syllabus.
      * @param principal the principal
      * @param syllabus the syllabus
      * @return syllabus optional
      */
+    @Operation(summary = "lists the syllabus",
+            description = " Can be invoked by auth users only",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "Listing the syllabus"),
+            @ApiResponse(responseCode = "204",
+                    description = "syllabus are not available"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials")})
+    @GetMapping
     public ResponseEntity<List<Syllabus>> list(final Principal
                                                      principal,
                                                            final
