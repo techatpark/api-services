b2857c4ba772008afe1f34a60971d2394c03d28d
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-04-07 19:39:22 +0530

    Seed Data

---
+++ gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO boards(title,description,created_by) VALUES('CBSE','Central Board','tom');
+INSERT INTO boards(title,description,created_by) VALUES('IGCSE','International General Certificate of Secondary Education','tom');
