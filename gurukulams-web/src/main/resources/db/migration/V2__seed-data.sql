70b3296d35d49cbaa3a3322b52e2d6c0c72a10bb
Swati-Khurana <swatitech015@gmail.com>
2022-04-08 12:59:22 +0530

    Master data for grades

--- gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
+++ gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
@@ -1,3 +1,11 @@
 
 INSERT INTO boards(title,description,created_by) VALUES('CBSE','Central Board','tom');
 INSERT INTO boards(title,description,created_by) VALUES('IGCSE','International General Certificate of Secondary Education','tom');
+
+INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'10 Standard','10 standard CBSE','tom');
+INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'11 Standard','11 standard CBSE','tom');
+INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'12 Standard','11 standard CBSE','tom');
+
+INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'1 Standard','1 standard IGCSE','tom');
+INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'2 Standard','2 standard IGCSE','tom');
+INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'3 Standard','3 standard IGCSE','tom');
