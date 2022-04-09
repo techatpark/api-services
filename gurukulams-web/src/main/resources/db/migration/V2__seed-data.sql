309c742ba0d0af46a2950bffce1e908bcd1c730a
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-04-09 07:59:38 +0530

    Add Seed Data

--- gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
+++ gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
@@ -2,10 +2,11 @@
 INSERT INTO boards(title,description,created_by) VALUES('CBSE','Central Board','tom');
 INSERT INTO boards(title,description,created_by) VALUES('IGCSE','International General Certificate of Secondary Education','tom');
 
-INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'10 Standard','10 standard CBSE','tom');
-INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'11 Standard','11 standard CBSE','tom');
-INSERT INTO grades(board_id,title,description,created_by) VALUES (1,'12 Standard','11 standard CBSE','tom');
+INSERT INTO grades(title,description,created_by) VALUES ('10 Standard','10 standard CBSE','tom');
+INSERT INTO grades(title,description,created_by) VALUES ('11 Standard','11 standard CBSE','tom');
+INSERT INTO grades(title,description,created_by) VALUES ('12 Standard','11 standard CBSE','tom');
 
-INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'1 Standard','1 standard IGCSE','tom');
-INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'2 Standard','2 standard IGCSE','tom');
-INSERT INTO grades(board_id,title,description,created_by) VALUES (2,'3 Standard','3 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('Maths','1 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('Physics','2 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('Chemistry','3 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('Biology','3 standard IGCSE','tom');
