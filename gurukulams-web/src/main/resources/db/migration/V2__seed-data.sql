19b07a859e52b25bd203c0a9b8628444ee5cbe3c
Manikanta445 <manikantayarramsetti445@gmail.com>
2022-04-09 11:34:30 +0530

    change

--- gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
+++ gurukulams-web/src/main/resources/db/migration/V2__seed-data.sql
@@ -6,7 +6,7 @@ INSERT INTO grades(title,description,created_by) VALUES ('10 Standard','10 stand
 INSERT INTO grades(title,description,created_by) VALUES ('11 Standard','11 standard CBSE','tom');
 INSERT INTO grades(title,description,created_by) VALUES ('12 Standard','11 standard CBSE','tom');
 
-INSERT INTO syllabus(title,description,created_by) VALUES ('Maths','1 standard IGCSE','tom');
-INSERT INTO syllabus(title,description,created_by) VALUES ('Physics','2 standard IGCSE','tom');
-INSERT INTO syllabus(title,description,created_by) VALUES ('Chemistry','3 standard IGCSE','tom');
-INSERT INTO syllabus(title,description,created_by) VALUES ('Biology','3 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('maths','1 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('physics','2 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('chemistry','3 standard IGCSE','tom');
+INSERT INTO syllabus(title,description,created_by) VALUES ('biology','3 standard IGCSE','tom');
