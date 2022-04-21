fe777960230d68f32f68ce89f2e39586850d6e0e
Swati-Khurana <swatitech015@gmail.com>
2022-04-21 11:24:06 +0530

    boards_grades_subjects integration check style fix

--- gurukulams-core/src/main/java/com/gurukulams/core/service/SubjectService.java
+++ gurukulams-core/src/main/java/com/gurukulams/core/service/SubjectService.java
@@ -1,7 +1,6 @@
 package com.gurukulams.core.service;
 
 import com.gurukulams.core.model.Subject;
-import com.gurukulams.core.model.Syllabus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
