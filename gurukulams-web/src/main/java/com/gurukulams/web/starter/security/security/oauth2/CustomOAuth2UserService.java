ef9e408faec600765479f55fe00158bd180bf2a5
haripriya <rm.haripriya@gmail.com>
2023-01-03 17:52:07 +0530

    Build Issues

--- gurukulams-web/src/main/java/com/gurukulams/web/starter/security/security/oauth2/CustomOAuth2UserService.java
+++ gurukulams-web/src/main/java/com/gurukulams/web/starter/security/security/oauth2/CustomOAuth2UserService.java
@@ -67,7 +67,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         final OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory
                 .getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration()
                         .getRegistrationId(), oAuth2User.getAttributes());
-        if (StringUtils.hasText(oAuth2UserInfo.getEmail())) {
+        if (!StringUtils.hasLength(oAuth2UserInfo.getEmail())) {
             throw new OAuth2AuthenticationProcessingException(
                     "Email not found from OAuth2 provider");
         }
